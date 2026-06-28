2d4fe52a892bf19a374889f857b8f5aeee1e47c7
Sharjeel Ashraf <sa062385@gmail.com>
2026-06-28 12:28:53 +0500

    Views.py is being under Processed

--- taskApp/views.py
+++ taskApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from taskApp.models import Category, Task
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def task_view(request):
