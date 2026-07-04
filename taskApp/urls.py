415e694c4208a410f87ac6c2b151427b4679307f
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-04 23:14:52 +0500

    Urls.py is Updated

--- taskApp/urls.py
+++ taskApp/urls.py
@@ -4,5 +4,6 @@ from taskApp import views
 
 urlpatterns = [
     path('task/', views.task_view, name='task'),
-    path('update/<taskID>/', views.update_task, name='update_task')
+    path('update/<taskID>/', views.update_task, name='update_task'),
+    path('delete/<taskID>/', views.delete_task, name='delete_task')
 ]
