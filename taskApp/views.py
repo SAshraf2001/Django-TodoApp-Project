6bfcde1938bcf4410b832c1724af3628c7717f07
Sharjeel Ashraf <sa062385@gmail.com>
2026-07-05 12:44:52 +0500

    HTTPResponse is Deleted

--- taskApp/views.py
+++ taskApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from taskApp.models import Category, Task
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
